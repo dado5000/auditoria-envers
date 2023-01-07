b61c02812585430fb25dc371b13410fa9d9889ab
Daverson Saldanha <dds.saldanha@gmail.com>
2023-01-07 17:05:01 -0300

    Add notação de auditoria

--- src/main/java/com/auditoria/envers/domain/model/Cliente.java
+++ src/main/java/com/auditoria/envers/domain/model/Cliente.java
@@ -8,14 +8,21 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+
+import org.hibernate.envers.AuditTable;
+import org.hibernate.envers.Audited;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 @Data
 @Entity
+@Table
+@Audited
+@AuditTable(value = "Cliente_audit")
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Cliente implements Serializable {
 	
